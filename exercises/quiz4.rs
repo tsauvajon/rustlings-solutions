d49cbb60db569a7fe2df4464e489066c12a14023
Thomas Sauvajon <thomas@sauvajon.tech>
2020-05-27 22:34:12 +1000

    First batch of exercices (#1)
    * Ex 1
    
    * Ex 2
    
    * Variables 3
    
    * Variables 4
    
    * Variables 5
    
    * If1
    
    * Functions1
    
    * Functions2
    
    * Functions3
    
    * Functions4
    
    * Functions5
    
    * Test1
    
    * PriviteTypes1
    
    * PriviteTypes2
    
    * PriviteTypes3
    
    * PriviteTypes4
    
    * PriviteTypes5
    
    * PriviteTypes6
    
    * Structs1
    
    * Structs2
    
    * Strings1
    
    * Strings2
    
    * Test2
    
    * Enums1
    
    * Enums2
    
    * Enums3
    
    * Tests1
    
    * Tests2
    
    * Tests3
    
    * Test3
    
    * Modules1
    
    * Modules2
    
    * Macros1
    
    * Macros2
    
    * Macros3
    
    * Macros4
    
    * Test4
    
    * MoveSementics1
    
    * MoveSementics2
    
    * MoveSementics3
    
    * MoveSementics4
    
    * Errors1
    
    * Errors2
    
    * Errors3
    
    * ErrorsN
    
    * Option1
    
    * Option2
    
    * Result1
    
    * Clippy1
    
    * Clippy2
    
    * Arc1
    
    * Iterators2
    
    * Iterators3
    
    * Iterators4

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -5,11 +5,15 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
-
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    macro_rules! my_macro {
+        ($val:expr) => {
+            "Hello ".to_owned() + $val
+        };
+    }
 
     #[test]
     fn test_my_macro_world() {
--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -7,14 +7,30 @@
 //         Try to ensure it returns a single string.
 // As always, there are hints if you execute `rustlings hint iterators2`!
 
-// I AM NOT DONE
-
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => first.collect::<String>() + c.as_str(),
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str(),
     }
+}
+
+pub fn capitalize_each_word(words: &Vec<&str>) -> Vec<String> {
+    let mut capitalized_words: Vec<String> = Vec::new();
+    for word in words.iter() {
+        capitalized_words.push(capitalize_first(&word))
+    }
+
+    capitalized_words
+}
+
+pub fn capitalize_into_string(words: &Vec<&str>) -> String {
+    let mut capitalized_words: Vec<String> = Vec::new();
+    for word in words.iter() {
+        capitalized_words.push(capitalize_first(&word))
+    }
+
+    capitalized_words.join("")
 }
 
 #[cfg(test)]
@@ -37,14 +53,14 @@ mod tests {
     #[test]
     fn test_iterate_string_vec() {
         let words = vec!["hello", "world"];
-        let capitalized_words: Vec<String> = // TODO
+        let capitalized_words: Vec<String> = capitalize_each_word(&words);
         assert_eq!(capitalized_words, ["Hello", "World"]);
     }
 
     #[test]
     fn test_iterate_into_string() {
         let words = vec!["hello", " ", "world"];
-        let capitalized_words = // TODO
+        let capitalized_words = capitalize_into_string(&words);
         assert_eq!(capitalized_words, "Hello World");
     }
 }
