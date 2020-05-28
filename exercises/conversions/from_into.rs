65e56584156b97c0bec2ddf049ce60767a660ba3
Thomas Sauvajon <thomas@sauvajon.tech>
2020-05-28 20:34:49 +1000

    FromInto

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -18,7 +18,6 @@ impl Default for Person {
     }
 }
 
-// I AM NOT DONE
 // Your task is to complete this implementation
 // in order for the line `let p = Person::from("Mark,20")` to compile
 // Please note that you'll need to parse the age component into a `usize`
@@ -35,6 +34,19 @@ impl Default for Person {
 // Otherwise, then return an instantiated Person object with the results
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let split: Vec<&str> = s.split(",").collect();
+        if split.len() != 2 {
+            Person::default()
+        } else if (split[0]).len() == 0 {
+            Person::default()
+        } else if let Ok(age) = (split[1]).parse::<usize>() {
+            Person {
+                name: String::from(split[0]),
+                age,
+            }
+        } else {
+            Person::default()
+        }
     }
 }
 
