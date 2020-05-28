ee8aa8e8c9bbebceed6336b765d8ddb4db633109
Thomas Sauvajon <thomas@sauvajon.tech>
2020-05-28 22:12:20 +1000

    FromStr

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -10,7 +10,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
 // Steps:
 // 1. If the length of the provided string is 0, then return an error
 // 2. Split the given string on the commas present in it
@@ -23,6 +22,20 @@ struct Person {
 impl FromStr for Person {
     type Err = String;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        let split: Vec<&str> = s.split(",").collect();
+        if split.len() != 2 {
+            Err(String::from("expected 2 args"))
+        } else if split[0].len() == 0 {
+            Err(String::from("missing name"))
+        } else {
+            match (split[1]).parse::<usize>() {
+                Err(_) => Err(String::from("couldn't parse age")),
+                Ok(age) => Ok(Person {
+                    name: String::from(split[0]),
+                    age,
+                }),
+            }
+        }
     }
 }
 
@@ -82,5 +95,4 @@ mod tests {
     fn missing_name_and_invalid_age() {
         ",one".parse::<Person>().unwrap();
     }
-
 }
